98819343aea344b722af868885f989ab5c1cac90
psarker69 <farukp2010@gmail.com>
2024-08-28 16:58:34 +0600

    Frontend Setup

---
+++ resources/views/frontend/layout/inc/script.blade.php
@@ -0,0 +1,26 @@
+<!-- jquery latest version -->
+<script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
+<!-- bootstrap js -->
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- owl.carousel.2.0.0-beta.2.4 css -->
+<script src="assets/js/owl.carousel.min.js"></script>
+<!-- scrollup.js -->
+<script src="assets/js/scrollup.js"></script>
+<!-- isotope.pkgd.min.js -->
+<script src="assets/js/isotope.pkgd.min.js"></script>
+<!-- imagesloaded.pkgd.min.js -->
+<script src="assets/js/imagesloaded.pkgd.min.js"></script>
+<!-- jquery.zoom.min.js -->
+<script src="assets/js/jquery.zoom.min.js"></script>
+<!-- countdown.js -->
+<script src="assets/js/countdown.js"></script>
+<!-- swiper.min.js -->
+<script src="assets/js/swiper.min.js"></script>
+<!-- metisMenu.min.js -->
+<script src="assets/js/metisMenu.min.js"></script>
+<!-- mailchimp.js -->
+<script src="assets/js/mailchimp.js"></script>
+<!-- jquery-ui.min.js -->
+<script src="assets/js/jquery-ui.min.js"></script>
+<!-- main js -->
+<script src="assets/js/scripts.js"></script>
---
+++ resources/views/frontend/layout/inc/style.blade.php
@@ -0,0 +1,23 @@
+<link rel="shortcut icon" type="image/png" href="assets/images/favicon.png">
+    <!-- Place favicon.ico in the root directory -->
+    <!-- all css here -->
+    <!-- bootstrap v4.0.0-beta.2 css -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- owl.carousel.2.0.0-beta.2.4 css -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <!-- font-awesome v4.6.3 css -->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <!-- flaticon.css -->
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <!-- jquery-ui.css -->
+    <link rel="stylesheet" href="assets/css/jquery-ui.css">
+    <!-- metisMenu.min.css -->
+    <link rel="stylesheet" href="assets/css/metisMenu.min.css">
+    <!-- swiper.min.css -->
+    <link rel="stylesheet" href="assets/css/swiper.min.css">
+    <!-- style css -->
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <!-- responsive css -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <!-- modernizr css -->
+    <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
